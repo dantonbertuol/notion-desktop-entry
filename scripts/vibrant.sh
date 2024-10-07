37dca2d9c37c644936403f3706f47908f2fde7ae
Danton Bertuol <32557284+dantonbertuol@users.noreply.github.com>
2024-10-07 18:53:29 -0300

    Update vibrant.sh

--- scripts/vibrant.sh
+++ scripts/vibrant.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 ~/Documents/dev/libvibrant/build/cli/vibrant-cli eDP-1 1.5
 ~/Documents/dev/libvibrant/build/cli/vibrant-cli HDMI-1 1.25
-xrandr --output eDP-1 --gamma .65:.65:.65
+xrandr --output eDP-1 --gamma .55:.55:.55
